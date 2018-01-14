5945e898cde53fecef1709755a01a885184fb424
Zack Mullaly <zsck@riseup.net>
2018-01-14 18:37:21 -0500

    Start fleshing out the implementation of the ask handler to start interacting with the database abstraction

--- server/src/api/questions/ask.rs
+++ server/src/api/questions/ask.rs
@@ -7,7 +7,7 @@ use iron::headers::ContentType;
 use iron::status;
 use serde_json as json;
 
-use models::Question;
+use models::{Question, Resource};
 use models::question::{QuestionRecord};
 
 
@@ -37,23 +37,43 @@ impl AskH {
 
 impl Handler for AskH {
     fn handle(&self, req: &mut Request) -> IronResult<Response> {
-        let request = req.get::<bodyparser::Struct<AskQuestionRequest>>().unwrap().unwrap();
+        let request = match req.get::<bodyparser::Struct<AskQuestionRequest>>() {
+            Ok(Some(request)) => request,
+            _ => {
+                let body = json::to_string(&AskQuestionResponse {
+                    error: Some("invalid request".to_string()),
+                    question: None,
+                })
+                .unwrap();
+                return Ok(Response::with((
+                    ContentType::json().0,
+                    status::BadRequest,
+                    body,
+                )));
+            }
+        };
+        let database = self.persistent_medium.lock().unwrap();
+        let mut question = Question {
+            id: "newquestion".to_string(),
+            presentation: request.presentation,
+            text: request.question,
+            nods: 0,
+            answered: false,
+            asked: "Just now".to_string(),
+        };
+        let (status_code, error) = match database.save(&mut question) {
+            Ok(_) => (status::Ok, None),
+            _     => (status::InternalServerError, Some("database communication error".to_string())),
+        };
+        let question = if error.is_some() { None } else { Some(question) };
         let body = json::to_string(&AskQuestionResponse {
-            error: None,
-            question: Some(Question {
-                id: "newquestion".to_string(),
-                presentation: request.presentation,
-                text: request.question,
-                nods: 0,
-                answered: false,
-                asked: "Just now".to_string(),
-            }),
+            error: error,
+            question: question,
         }).unwrap();
         Ok(Response::with((
             ContentType::json().0,
-            status::Ok,
+            status_code,
             body,
         )))
     }
 }
-
