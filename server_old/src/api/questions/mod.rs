ba00213c2452d496b77029bd9f0c0564f4dd989c
Zack Mullaly <zsck@riseup.net>
2018-02-06 15:19:48 -0500

    Don't need the old server code anymore

--- server_old/src/api/questions/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod list;
-pub mod ask;
-pub mod nod;
-
-pub use self::list::ListH;
-pub use self::ask::AskH;
-pub use self::nod::NodH;
--- server_old/src/models/error.rs
+++
@@ -1,8 +0,0 @@
-use std::error::Error;
-
-
-/// Represents the possible errors that may need to be handled when interacting with a persistence medium.
-#[derive(Debug)]
-pub enum ModelError {
-    DatabaseError(Box<Error>),
-}
